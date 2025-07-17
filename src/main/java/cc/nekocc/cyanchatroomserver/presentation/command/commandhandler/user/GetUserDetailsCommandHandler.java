618eef75b1c23cde40a5ce70c3e8964db3797c3e
Ext Culith <unirzine@gmail.com>
2025-07-18 01:12:14 +0800

    ADD friendship request handling logic and improve storage directory initialization.

--- src/main/java/cc/nekocc/cyanchatroomserver/presentation/command/commandhandler/user/GetUserDetailsCommandHandler.java
+++ src/main/java/cc/nekocc/cyanchatroomserver/presentation/command/commandhandler/user/GetUserDetailsCommandHandler.java
@@ -40,7 +40,8 @@ public class GetUserDetailsCommandHandler implements CommandHandler
                 boolean is_key_enabled = key_management_service_.fetchKeys(user.getId()).isPresent();
 
                 GetUserDetailsResponse user_response = new GetUserDetailsResponse(
-                        payload.client_request_id(), true, user.getUsername(), user.getNickname(), user.getAvatarUrl(),
+                        payload.client_request_id(), true, user.getUsername(),
+                        user.getId(), user.getNickname(), user.getAvatarUrl(),
                         user.getSignature(), user.getStatus(), is_online, is_key_enabled);
                 ProtocolMessage<GetUserDetailsResponse> response_msg = new ProtocolMessage<>("GET_USER_DETAILS_RESPONSE", user_response);
                 ctx.channel().writeAndFlush(new TextWebSocketFrame(JsonUtil.serialize(response_msg)));
@@ -48,7 +49,7 @@ public class GetUserDetailsCommandHandler implements CommandHandler
             {
                 GetUserDetailsResponse user_response = new GetUserDetailsResponse(
                         payload.client_request_id(), false, null, null, null,
-                        null, null, false, false);
+                        null, null, null, false, false);
                 ProtocolMessage<GetUserDetailsResponse> response_msg = new ProtocolMessage<>("GET_USER_DETAILS_RESPONSE", user_response);
                 ctx.channel().writeAndFlush(new TextWebSocketFrame(JsonUtil.serialize(response_msg)));
             }
--- src/main/java/cc/nekocc/cyanchatroomserver/presentation/dto/response/GetUserDetailsResponse.java
+++ src/main/java/cc/nekocc/cyanchatroomserver/presentation/dto/response/GetUserDetailsResponse.java
@@ -3,6 +3,7 @@ package cc.nekocc.cyanchatroomserver.presentation.dto.response;
 import cc.nekocc.cyanchatroomserver.domain.model.user.UserStatus;
 import java.util.UUID;
 
-public record GetUserDetailsResponse(UUID client_request_id, boolean request_status, String username, String nick_name, String avatar_url,
-                                     String signature, UserStatus status, boolean is_online, boolean enabled_key)
+public record GetUserDetailsResponse(UUID client_request_id, boolean request_status, String username, UUID user_id,
+                                     String nick_name, String avatar_url, String signature, UserStatus status,
+                                     boolean is_online, boolean enabled_key)
 {  }
